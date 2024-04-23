7c545ae18f301b9faeb5f268d48fb3a45bb731e1
ismaelromero104 <166445372+ismaelromero104@users.noreply.github.com>
2024-04-23 11:38:08 -0500

    Add files via upload

---
+++ verify-part-1.sh
@@ -0,0 +1,25 @@
+cd
+pwd
+mkdir ~/playground
+ mkdir ~/playground/usa
+ mkdir ~/playground/canada
+ mkdir ~/playground/usa/ohio
+ mkdir ~/playground/usa/pennsylvania
+ mkdir ~/playground/usa/michigan
+ mkdir ~/playground/canada/quebec
+ mkdir ~/playground/canada/british-columbia
+ mkdir ~/playground/usa/ohio/cuyahoga
+ mkdir ~/playground/usa/ohio/hamilton
+ mkdir ~/playground/usa/ohio/franklin
+ mkdir ~/playground/usa/pennsylvania/allegheny
+ mkdir ~/playground/usa/michigan/wayne
+ touch ~/playground/usa/ohio/cuyahoga/cleveland.txt
+ touch ~/playground/usa/ohio/cuyahoga/cincinnati.txt
+ mv ~/playground/usa/ohio/cuyahoga/cincinnati.txt ~/playground/usa/ohio/hamilton
+ cp ~/playground/usa/ohio/cuyahoga/cleveland.txt ~/playground/usa/ohio/franklin/columbus.txt
+ touch ~/playground/usa/pennsylvania/allegheny/pittsburgh.txt
+ touch ~/playground/usa/michigan/wayne/detroit.txt
+ touch ~/playground/canada/quebec/montreal.txt
+ touch ~/playground/canada/quebec/quebec-city.txt
+ touch ~/playground/canada/british-columbia/vancouver.txt
+ touch ~/playground/canada/british-columbia/prince-george.txt
